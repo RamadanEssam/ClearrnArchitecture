d728a3723de862681662f3af0702ea23e5096e14
Ramadan <r.eita@wesamsoft.com>
2022-06-14 17:35:15 +0200

    commit Implementation of services and Repository GetCourses

---
+++ CleanArch/CleanArch.Infra.Date/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Date.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Date.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private  UniversityDBContext _ctx;
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}

50f00cfd6fc88087d217fb03bf14a7962346e993
Ramadan <r.eita@wesamsoft.com>
2022-06-14 11:03:54 +0200

    Edit Domain

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -10,7 +10,8 @@ namespace CleanArch.Domain.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public string Description { get; set; }
+       
     }
 }
